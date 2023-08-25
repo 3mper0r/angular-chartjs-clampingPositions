ae4c501935a5d59c6e71b03ad6371283b82f2150
Elidon Morina <83674499+3mper0r@users.noreply.github.com>
2023-08-25 16:45:39 +0200

    fix: don't show 0h when is less then 60m

--- src/app/Pipes/decimal-to-time.pipe.ts
+++ src/app/Pipes/decimal-to-time.pipe.ts
@@ -11,7 +11,10 @@ export class DecimalToTimePipe implements PipeTransform {
     const hours = Math.floor(totalSeconds / 3600)
     const minutes = Math.floor((totalSeconds % 3600) / 60)
     const seconds = totalSeconds % 60
-    return `${hours.toString().padStart(1, '0')}h ${minutes.toString().padStart(2, '0')}m ${seconds.toString().padStart(2, '0')}s`;
-  }
 
+    return hours > 0 ? `${hours.toString().padStart(1, '0')}h ${minutes.toString().padStart(2, '0')}m ${seconds.toString().padStart(2, '0')}s` : `${minutes.toString().padStart(2, '0')}m ${seconds.toString().padStart(2, '0')}s`
+    // if(hours > 0)
+    // return `${hours.toString().padStart(1, '0')}h ${minutes.toString().padStart(2, '0')}m ${seconds.toString().padStart(2, '0')}s`;
+    // return `${minutes.toString().padStart(2, '0')}m ${seconds.toString().padStart(2, '0')}s`
+  }
 }
