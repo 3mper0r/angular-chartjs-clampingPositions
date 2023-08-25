a2de3aa7a8249a656a4ad7d81033061dd5d2e91f
Elidon Morina <83674499+3mper0r@users.noreply.github.com>
2023-08-25 16:25:55 +0200

    feature: Pipe decimalToTime

--- src/app/Pipes/decimal-to-time.pipe.ts
+++ src/app/Pipes/decimal-to-time.pipe.ts
@@ -7,11 +7,11 @@ export class DecimalToTimePipe implements PipeTransform {
 
   transform(decimalNumber: number): string {
 
-    const totalSeconds = Math.floor(decimalNumber * 3600)
+    const totalSeconds = Math.floor(decimalNumber)
     const hours = Math.floor(totalSeconds / 3600)
     const minutes = Math.floor((totalSeconds % 3600) / 60)
     const seconds = totalSeconds % 60
-    return `${hours.toString().padStart(2, '0')}h ${minutes.toString().padStart(2, '0')}m ${seconds.toString().padStart(2, '0')}s`;
+    return `${hours.toString().padStart(1, '0')}h ${minutes.toString().padStart(2, '0')}m ${seconds.toString().padStart(2, '0')}s`;
   }
 
 }
