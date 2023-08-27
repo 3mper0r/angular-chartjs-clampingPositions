25ad77d607babe34933fa1c6a6c4b0f9d01e5e9b
Elidon Morina <83674499+3mper0r@users.noreply.github.com>
2023-08-27 12:30:30 +0200

    first canvas render

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,125 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, Inject, Injectable, OnInit } from '@angular/core';
 import werkZeugData from '../data/data.json'
 import { WerkzeugData } from './models/werkzeug-data';
 import { Chart } from 'chart.js';
-import { ChartDataComponent } from './chart-data/chart-data.component';
+import { registerables } from 'chart.js';
+import { isNullOrUndef } from 'chart.js/dist/helpers/helpers.core';
+import { transpose } from 'd3';
+Chart.register(...registerables);
+//import * as echarts from 'echarts'
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements AfterViewInit  {
   title = 'first-angular-app';
+  
   //newMemberName: string = ''
   //names: string[] = []
   //errorMessage: string = ''
   
+  chartData: number[][] = []
+  data: WerkzeugData[] = werkZeugData;
+  cposmin: any  
+  cposmax: any 
 
   
-  myChart: any
-  data: WerkzeugData[] = werkZeugData;
 
+  // createChartJsBar(minValue: number, maxValue: number) {
+  //   werkZeugData.map(data => {
+  //     this.chartData.push([data.cposmin, data.cposmax])
+  //   });
+  // }
+  
+  myChart: any 
+  ngAfterViewInit() {
+
+    const minValues = this.data.map(min => min.cposmin)
+    const maxValues = this.data.map(max => max.cposmax)
+
+    //const diff = maxValues[] - minValues
+
+    //const covalue = this.data.forEach(diff => diff.cposmax - diff.cposmin)
+
+    const ctx = document.getElementById('myChart') as HTMLCanvasElement;
+    const myChart = new Chart('myChart', {
+      type: 'bar',
+      data: {
+        labels: [''],
+        datasets: [{
+          label: '',
+          data: [minValues, maxValues],
+          backgroundColor: 'rgba(186, 220, 154)',
+          borderColor: 'rgba(131, 195, 62)',
+          borderWidth: 1,
+          borderSkipped: false,
+          barPercentage: 30,
+          barThickness: 30,
+          minBarLength: 30
+          
+        }]
+      },
+      options: {
+        
+        indexAxis: 'y',       
+      
+        scales: {
+          x: {
+            
+            stacked: true,
+            beginAtZero: false,
+            min: 16000,
+            max: 22000,
+            display: true,
+            grid: {
+              display:false,
+            },
+            ticks: {
+              mirror: true,
+              stepSize: 1,
+              display: false, 
+              
+            },
+              
+          },
+          y: {
+            stacked: true,
+            display: true,
+            
+          }
+
+        }
+      }
+    });
+    
+  }
+ 
+    
+//   const ctx = document.getElementById('myChart');
+
+//   new Chart('myChart', {
+//     type: 'bar',
+//     data: {
+//       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//       datasets: [{
+//         label: '# of Votes',
+//         data: [12, 19, 3, 5, 2, 3],
+//         borderWidth: 1
+//       }]
+//     },
+//     options: {
+//       scales: {
+//         y: {
+//           beginAtZero: true
+//         }
+//       }
+//     }
+//   });
+
+  
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,27 @@
-import { NgModule } from '@angular/core';
+import { Inject, Injectable, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { DecimalToTimePipe } from './Pipes/decimal-to-time.pipe';
-import { ChartDataComponent } from './chart-data/chart-data.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     DecimalToTimePipe,
-    ChartDataComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    NgxChartsModule,
   ],
-  providers: [],
+  providers:[],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
